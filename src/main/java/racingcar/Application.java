14741ab1f3056c93220856ab6157bb00c3d1c60e
Jinseong <jinsung528@gmail.com>
2025-10-21 23:25:56 +0900

    feat(CarNameParser): 자동차 이름 파싱 기능 구현
    - 쉼표(,)로 구분된 문자열을 분리하여 List<String>으로 변환하는 코드 작성
    - Application에 입력받은 문자열을 파싱하는 임시 코드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,17 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.util.CarNameParser;
 import racingcar.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 이후 수정
         InputView inputView = new InputView();
-        String carName = inputView.readCarName();
+        String carNames = inputView.readCarName();
         String tryCount = inputView.readTryCount();
+
+        CarNameParser parser = new CarNameParser();
+        List<String> carNameList = parser.parseCarNames(carNames);
     }
 }
