5022f49c2f98ce10e25017f08ee8570a19e0707b
Jinseong <jinsung528@gmail.com>
2025-10-22 16:56:23 +0900

    feat(CarNameParser): 자동차 이름 공백 검증 추가
    - 자동차 이름 입력 문자열이 비었거나 공백만 있는 경우 예외 발생

--- src/main/java/racingcar/util/CarNameParser.java
+++ src/main/java/racingcar/util/CarNameParser.java
@@ -5,6 +5,14 @@ import java.util.List;
 
 public class CarNameParser {
     public List<String> parse(String input) {
+        checkEmpty(input);
+
         return Arrays.stream(input.split(",")).toList();
+    }
+
+    private void checkEmpty(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름을 입력해야 합니다.");
+        }
     }
 }
