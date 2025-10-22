f75804cddcc6be61f5e429985e594f816e0de9b1
Jinseong <jinsung528@gmail.com>
2025-10-22 16:41:18 +0900

    feat(TryCountValidator): 시도 횟수 최대값 검증 기능 구현
    - 시도 횟수가 100 초과일 경우 예외 발생
    - README에 시도 횟수 최대값 정보 반영

--- src/main/java/racingcar/util/TryCountValidator.java
+++ src/main/java/racingcar/util/TryCountValidator.java
@@ -3,14 +3,22 @@ package racingcar.util;
 public class TryCountValidator {
 
     private static final int MIN_TRY_COUNT = 1;
+    private static final int MAX_TRY_COUNT = 100;
 
     public void validate(int tryCount) {
         validateMin(tryCount);
+        validateMax(tryCount);
     }
 
     private void validateMin(int tryCount) {
         if (tryCount < MIN_TRY_COUNT) {
             throw new IllegalArgumentException("시도 횟수는 최소 " + MIN_TRY_COUNT + " 이상이어야 합니다");
+        }
+    }
+
+    private void validateMax(int tryCount) {
+        if (tryCount > MAX_TRY_COUNT) {
+            throw new IllegalArgumentException("시도 횟수는 최대 " + MAX_TRY_COUNT + " 이하이어야 합니다");
         }
     }
 }
